9fcf0fe295c4debe66dd8010600b80ca50ab490b
jacob <j.bartoszek.jb@gmail.com>
2022-10-04 18:01:21 +0200

    deleted build directories

--- 11testing/build/googletest-main/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jacob/Code/CPP/Apriorit/11testing/googletest-main
-# Build directory: /home/jacob/Code/CPP/Apriorit/11testing/build/googletest-main
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
